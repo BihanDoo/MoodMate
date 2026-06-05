08d4685b42b0e54247dd1d66af6d98df43ae0bff
Cheez <sathmirabihandu@gmail.com>
2026-06-05 05:27:49 -0700

    updated login state behaviour

--- app/src/main/java/lk/scu/moodmate/MainActivity.java
+++ app/src/main/java/lk/scu/moodmate/MainActivity.java
@@ -1,6 +1,7 @@
 package lk.scu.moodmate;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,8 +12,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-    Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +25,24 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
-
-
-
     public void onbtncatclick(View view) {
-        Intent i = new Intent();
-        i.setClass(this, cat.class);
+        Intent i = new Intent(this, cat.class);
         startActivity(i);
-
     }
 
     public void onbtnjoke(View view) {
-        Intent i = new Intent();
-        i.setClass(this, Joke.class);
+        Intent i = new Intent(this, Joke.class);
         startActivity(i);
+    }
+
+    public void onLogoutClick(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences("UserSession", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+
+        Intent intent = new Intent(MainActivity.this, login.class);
+        startActivity(intent);
+        finish();
     }
 }
